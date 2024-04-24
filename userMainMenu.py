fba4dffc162dd48aef7d37f51e20f8addffce499
jakelinelinares <158516605+jakelinelinares@users.noreply.github.com>
2024-04-24 15:43:00 -0400

    Updated userMainMenu.py

--- userMainMenu.py
+++ userMainMenu.py
@@ -12,8 +12,7 @@ userMainMenu.title('User Main Menu')
 userMainMenu.geometry('1920x1080')
 userMainMenu.config(bg='light blue')
 
-# temp for testing until i can connect to login user
-username = "jakie"
+print('test username' + username)
 
 userMainMenu.grid_rowconfigure(0, weight=1)
 userMainMenu.grid_columnconfigure(0, weight=1)
@@ -22,37 +21,39 @@ db = mysql.connector.connect(host="localhost", user="root", password="Jackie2013
 
 cursor = db.cursor()
 
-
 # Welcome
-LT = tk.Label(userMainMenu, text="Welcome!", font=('Times', 50))
+LT = tk.Label(userMainMenu, text=f"Welcome {username}! ", font=('Times', 50))
 LT.grid(row=0, column=0)
-# LT.pack(side=TOP, expand=True, fill=None)
-# LT.pack(expand=True)
 LT.config(bg="light blue", fg="white")
+
 
 # Menu Intro
 LT0 = tk.Label(userMainMenu, text="Main Menu", font=('Times', 25))
-# LT0.pack(side=TOP, expand=True)
 LT0.grid(row=1, column=0)
 LT0.config(bg="light blue", fg="white")
 
-# Profile display
+# Diary function
+def diary():
+    # connect ot the database
+    db = mysql.connector.connect(host="localhost", user="root", password="Jackie2013", database="diary_management")
 
-# username
-LT2 = tk.Label(userMainMenu, text=username, font=('Times', 20))
-# LT2.pack(side=tk.TOP, expand=False, fill=None)
-LT2.grid(row=2, column=0)
-LT2.config(bg="light blue", fg="White")
+    # create cursor
+    cursor = db.cursor()
+
+    userMainMenu.destroy()
+    print('made it past userMainMenu.destroy()')
+    exec(open("diary.py").read())
+    print('opened diary.py')
+
+    # Commit Changes
+    db.commit()
+    # Close Connection
+    db.close()
 
 
-# # user_id
-# def get_userID():
-#     user_id = cursor.execute("""Select User_ID from Users where Username = %s""", (username,))
-#     userIdResult = cursor.fetchall()
-#
-#     LT1 = tk.Label(userMainMenu, text=userIdResult, font=('Times', 20))
-#     # LT1.pack(side=tk.TOP, expand=False, fill=None)
-#     LT1.config(bg="light blue", fg="White")
+# diary button
+diaryBtn = Button(userMainMenu, text="Open Diary", command=diary)
+diaryBtn.grid(row=3, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 
 # Record function
@@ -92,7 +93,7 @@ def viewRecords():
 
 # record button
 viewRecordsBtn = Button(userMainMenu, text="View Records", command=viewRecords)
-viewRecordsBtn.grid(row=3, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+viewRecordsBtn.grid(row=4, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 
 # create record function
@@ -116,7 +117,7 @@ def createRecords():
 
 # create record button
 createRecordsBtn = Button(userMainMenu, text="Create Records", command=createRecords)
-createRecordsBtn.grid(row=4, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+createRecordsBtn.grid(row=5, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 
 # admin function
@@ -152,7 +153,7 @@ def viewAdmins():
 
 # admin button
 viewAdminsBtn = Button(userMainMenu, text="View Admins", command=viewAdmins)
-viewAdminsBtn.grid(row=5, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+viewAdminsBtn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 
 # planner function
@@ -171,7 +172,7 @@ def viewPlanner():
 
 # planner button
 viewPlannerBtn = Button(userMainMenu, text="View Planner", command=viewPlanner)
-viewPlannerBtn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+viewPlannerBtn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 
 # group function
@@ -211,7 +212,7 @@ def viewGroups():
 
 # view group button
 viewPlannerBtn = Button(userMainMenu, text="View Groups", command=viewGroups)
-viewPlannerBtn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+viewPlannerBtn.grid(row=8, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 
 def createGroups():
@@ -231,9 +232,11 @@ def createGroups():
     # Close Connection
     db.close()
 
+
 # create group button
 createGroupsBtn = Button(userMainMenu, text="Create Groups", command=createGroups)
-createGroupsBtn.grid(row=8, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+createGroupsBtn.grid(row=9, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+
 
 # logout function
 def logout():
@@ -251,77 +254,7 @@ def logout():
 
 # logout button
 logoutButton = Button(userMainMenu, text="Logout", command=logout)
-logoutButton.grid(row=9, column=0)
-
-# logoutButton = tk.Button(text="Logout", width=15, height=2, bg="blue", fg="black")
-# logoutButton.pack(side=tk.BOTTOM, expand=False, fill=None)
-
+logoutButton.grid(row=10, column=0)
 
 print(db.is_connected())
 userMainMenu.mainloop()
-
-# # Group
-# groupValues = ["View Groups", "Group Requests", "Create Group"]
-#
-# variable2 = StringVar(userMainMenu)
-# variable2.set("Group")
-#
-# g = OptionMenu(userMainMenu, variable2, *groupValues)
-# # g.pack(side=tk.TOP, expand=False, fill=None)
-# g.grid(row=8, column=0)
-# g.config(width=34, height=4, bg="light blue", fg="black")
-
-# Admin
-# adminValues = ["View Admins", "Admin Requests"]
-#
-# variable3 = StringVar(userMainMenu)
-# variable3.set("Admin")
-#
-# a = OptionMenu(userMainMenu, variable3, *adminValues)
-# # a.pack(side=tk.TOP, expand=False, fill=None)
-# a.grid(row=9, column=0)
-# a.config(width=34, height=4, bg="light blue", fg="black")
-
-# # Planner
-# plannerValues = ["Planner Page"]
-#
-# variable1 = StringVar(userMainMenu)
-# variable1.set("Planner")
-#
-# p = OptionMenu(userMainMenu, variable1, *plannerValues)
-# # p.pack(side=tk.TOP, expand=False, fill=None)
-# p.grid(row=7, column=0)
-# p.config(width=33, height=4, bg="light blue", fg="black")
-
-# admin status
-# def get_adminStatus():
-#     adminQuery = cursor.execute("""select Has_Admin from Users where Username = %s""", (username,))
-#     adminResult = cursor.fetchall()
-#
-#     if adminResult == 'yes':
-#         findAdminQuery = cursor.execute(
-#             """select Admin_Name from Admins where admin_ID =(Select admin_id from Users where Username = %s""",
-#             (username,))
-#
-#         LT3 = tk.Button(userMainMenu, text=findAdminQuery, font=('Times', 20))
-#         LT3.pack(side=tk.TOP, expand=False, fill=None)
-#         LT3.config(bg="light blue", fg="White")
-#         # adminLabel = Label(userMainMenu, text=str(findAdminQuery))
-#
-#     else:
-#         LT4 = tk.Button(userMainMenu, text='none', font=('Times', 20))
-#         LT4.pack(side=tk.TOP, expand=False, fill=None)
-#         LT4.config(bg="light blue", fg="White")
-
-# def get_username():
-#     username_query = cursor.execute("""Select Username from Users where User_ID = pass""", (username,))
-#     usernameResult = cursor.fetchall()
-
-# recordValues = ["View Records", "Archive Records"]
-# variable = StringVar(userMainMenu)
-# variable.set("Records")
-
-# r = OptionMenu(userMainMenu, variable, *recordValues)
-# # r.pack(side=tk.TOP, expand=False, fill=None)
-# r.grid(row=6, column=0)
-# r.config(width=33, height=4, bg="light blue", fg="black")
